b6fc993419aac2c85d417ae2aef96007d2096909
Timothy Oliver <oliverwakogi@live.com>
2019-07-11 22:15:30 +0300

    Login form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import './App.css'
 import NavBar from './components/Navbar'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  }
+})
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Router>
-          <NavBar/>
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={SignUp} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Router>
+            <NavBar/>
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/signup" component={SignUp} />
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,150 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import withStyles from '@material-ui/core/styles/withStyles'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import axios from 'axios'
+import PropTypes from 'prop-types'
+import AppIcon from '../images/icon.png'
+import { positions } from '@material-ui/system';
+
+const styles = {
+  grid: {
+    textAlign: 'center'
+  },
+  image: {
+    margin: '20px auto 20px auto'
+  },
+  pageTitle: {
+    margin: '10px auto 10px auto'
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  textField: {
+    margin: '0px 0px 10px 0px'
+  },
+  button: {
+    marginTop: '20px',
+    position: 'relative'
+  },
+  customError: {
+    color: 'red',
+    fontSize: '0.8rem',
+    marginTop: '10px'
+  },
+  progress: {
+    position: 'absolute'
+  }
+}
 
 class Login extends Component {
+  constructor() {
+    super()
+    this.state = {
+      email: '',
+      password: '',
+      loading: false,
+      errors: {},
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({ loading: true })
+    const userData = {
+      email: this.state.email,
+      password: this.state.password,
+    }
+    axios
+      .post('/login', userData)
+      .then(res => {
+        console.log(res.data)
+        this.setState({ loading: false })
+        this.props.history.push('/')
+      })
+      .catch(err => {
+        this.setState({
+          errors: err.response.data,
+          loading: false
+        })
+      })
+  }
+
+  handleChange = event => {
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value
+    })
+  }
+
   render() {
+    const { classes } = this.props
+    const { errors, loading } = this.state
+
     return (
-      <div>
-        <h1>Login Page</h1>
-      </div>
+      <Grid container className={classes.grid}>
+        <Grid item sm/>
+        <Grid item sm>
+          <img src={AppIcon} alt="monkey logo" className={classes.image}/>
+          <Typography variant="h2" className={classes.pageTitle}>
+            Login
+          </Typography>
+          <form noValidate onSubmit={this.handleSubmit} className={classes.form}>
+            <TextField 
+              id="email"
+              name="email"
+              type="email"
+              label="email"
+              className={classes.textField}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullwidth
+              helperText = {errors.email}
+              error={errors.email ? true : false}
+            />
+            <TextField 
+              id="password"
+              name="password"
+              type="password"
+              label="password"
+              className={classes.textField}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullwidth
+              helperText = {errors.password}
+              error={errors.password ? true : false}
+            />
+            {errors.general &&
+              <Typography variant="body2" className={classes.customError}>{errors.general}</Typography>
+            }
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              disabled={loading}
+            >
+              Log in
+              {loading && (
+                <CircularProgress size={30} className={classes.progress} />
+              )}
+            </Button>
+            <small>don't have an account? sign up <Link to="/signup">here</Link></small>
+          </form>
+        </Grid>
+        <Grid item sm/>
+      </Grid>
     )
   }
 }
 
-export default Login
+Login.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Login)
